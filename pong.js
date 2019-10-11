c9355c907411ab06695f1e0fc84a77211369dd29
lyzengar <darthmaul85@hotmail.com>
2019-10-10 18:16:54 -0700

    draw background

--- pong.js
+++ pong.js
@@ -4,4 +4,11 @@ var canvasContext;
 window.onload = function() {
     canvas = document.getElementById('gameCanvas');
     canvasContext = canvas.getContext('2d');
+
+    drawEverything();
+}
+
+function drawEverything() {
+    canvasContext.fillStyle = 'black';
+    canvasContext.fillRect(0, 0, canvas.clientWidth, canvas.height);
 }

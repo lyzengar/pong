8c20418a9ee006040e477f98985ecaa05af7a3ac
lyzengar <darthmaul85@hotmail.com>
2019-10-13 09:56:40 -0700

    add circle

--- pong.js
+++ pong.js
@@ -9,6 +9,13 @@ window.onload = function() {
 }
 
 function drawEverything() {
+    // background
     canvasContext.fillStyle = 'black';
     canvasContext.fillRect(0, 0, canvas.clientWidth, canvas.height);
+
+    // draw a circle
+    canvasContext.fillStyle = 'white';
+    canvasContext.beginPath();
+    canvasContext.arc(75, 75, 10, 0, Math.PI*2, true);
+    canvasContext.fill();
 }

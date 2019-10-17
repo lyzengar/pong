16663db0a344e3296d03d20f5de2b5c4e64fba9a
lyzengar <darthmaul85@hotmail.com>
2019-10-17 15:19:17 -0700

    add mouse event listener in js

--- pong.js
+++ pong.js
@@ -46,3 +46,15 @@ function drawEverything() {
     // draw the ball
     colorCircle(ballX, ballY, 10, 'white');
 }
+
+function calculateMousePos(evt) {
+    var rect = canvas.getBoundingClientRect(), root = document.documentElement;
+
+    // account for the margins, canvas position on page, scroll amount, etc.
+    var mouseX = evt.clientX - rect.left - root.scrollLeft;
+    var mouseY = evt.clientY - rect.top - root.scrollTop;
+    return {
+        x: mouseX,
+        y: mouseY
+    };
+}

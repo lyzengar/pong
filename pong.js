432d33d883153ce9501b49b56ab5298f20c265f2
lyzengar <darthmaul85@hotmail.com>
2019-10-17 14:56:57 -0700

    update circle helper

--- pong.js
+++ pong.js
@@ -30,7 +30,10 @@ function colorRect(topLeftX, topLeftY, boxWidth, boxHeight, fillColor) {
 }
 
 function colorCircle(centerX, centerY, radius, fillColor) {
-    
+    canvasContext.fillStyle = fillColor;
+    canvasContext.beginPath();
+    canvasContext.arc(centerX, centerY, radius, 0, Math.PI*2, true);
+    canvasContext.fill();
 }
 
 function drawEverything() {
